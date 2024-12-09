59b6da5daba5e698387b8241b70f5a8eb6ef3b75
michaelwellens <michael@airobot.eu>
2024-12-09 17:47:14 +0100

    expanded the lstm model with more layres, dynamic learning rates, added dropout layers and added regularizers

--- LSTM/LSTM_euromillions.py
+++ LSTM/LSTM_euromillions.py
@@ -3,7 +3,10 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from keras import layers, regularizers, models
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from keras.optimizers import Adam
+from tensorflow.keras.models import load_model
 from art import text2art
 
 path = os.getcwd()
@@ -36,19 +39,68 @@ def load_data():
 # Function to create the model
 def create_model(num_features, max_value):
     # Create a sequential model
-    model = keras.Sequential()
-    # Add an Embedding layer, LSTM layer, and Dense layer to the model
-    model.add(layers.Embedding(input_dim=max_value+1, output_dim=64))
-    model.add(layers.LSTM(256))
+    model = models.Sequential()
+    
+    # Add an Embedding layer
+    model.add(layers.Embedding(input_dim=max_value + 1, output_dim=64, input_length=None))
+
+    # Add the first LSTM layer with L2 regularization
+    model.add(layers.LSTM(256, return_sequences=True, 
+                          kernel_regularizer=regularizers.l2(0.001), 
+                          recurrent_regularizer=regularizers.l2(0.001)))
+    
+    # Add a Dropout layer
+    model.add(layers.Dropout(0.1))
+
+    # Add the first LSTM layer with L2 regularization
+    model.add(layers.LSTM(128, return_sequences=True, 
+                          kernel_regularizer=regularizers.l2(0.001), 
+                          recurrent_regularizer=regularizers.l2(0.001)))
+    
+    # Add a Dropout layer
+    model.add(layers.Dropout(0.1))
+    
+    # Add the first LSTM layer with L2 regularization
+    model.add(layers.LSTM(64, return_sequences=True, 
+                          kernel_regularizer=regularizers.l2(0.001), 
+                          recurrent_regularizer=regularizers.l2(0.001)))
+    
+    # Add a Dropout layer
+    model.add(layers.Dropout(0.1))
+    
+    # Add a second LSTM layer with L2 regularization
+    model.add(layers.LSTM(32, return_sequences=True,
+                          kernel_regularizer=regularizers.l2(0.001), 
+                          recurrent_regularizer=regularizers.l2(0.001)))
+    
+    # Add another Dropout layer
+    model.add(layers.Dropout(0.1))
+    
+    # Add a second LSTM layer with L2 regularization
+    model.add(layers.LSTM(16, 
+                          kernel_regularizer=regularizers.l2(0.001), 
+                          recurrent_regularizer=regularizers.l2(0.001)))
+    
+    # Add another Dropout layer
+    model.add(layers.Dropout(0.1))
+    
+    # Add a Dense layer for output
     model.add(layers.Dense(num_features, activation='softmax'))
+    
     # Compile the model with categorical crossentropy loss, adam optimizer, and accuracy metric
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.00001), metrics=['accuracy'])
+    
     return model
 
 # Function to train the model
 def train_model(model, train_data, val_data):
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=1e-6)
+    checkpoint = ModelCheckpoint(os.path.join(os.path.abspath(os.path.join(path, os.pardir)), "data", "lstm_model", "model_euromillions_checkpoint.keras"), save_best_only=True)
+
     # Fit the model on the training data and validate on the validation data for 100 epochs
-    history = model.fit(train_data, train_data, validation_data=(val_data, val_data), epochs=1000)
+    history = model.fit(train_data, train_data, validation_data=(val_data, val_data), epochs=1000, batch_size=512, callbacks=[early_stopping, reduce_lr, checkpoint])
+
 
 # Function to predict numbers using the trained model
 def predict_numbers(model, val_data, num_features):
@@ -79,9 +131,14 @@ def main():
    
    # Get number of features from training data 
    num_features = train_data.shape[1]
+
+   if os.path.exists(os.path.join(os.path.abspath(os.path.join(path, os.pardir)), "data", "lstm_model", "model_euromillions")):
+       model = load_model(os.path.join(os.path.abspath(os.path.join(path, os.pardir)), "data", "lstm_model", "model_euromillions"))
+   else:
+        # Create and compile model 
+       model = create_model(num_features, max_value)
    
-   # Create and compile model 
-   model = create_model(num_features, max_value)
+   
    
    # Train model 
    train_model(model, train_data, val_data)
@@ -91,6 +148,8 @@ def main():
    
    # Print predicted numbers 
    print_predicted_numbers(predicted_numbers)
+
+   model.save(os.path.join(os.path.abspath(os.path.join(path, os.pardir)), "data", "lstm_model", "model_euromillions"))
 
 # Run main function if this script is run directly (not imported as a module)
 if __name__ == "__main__":
