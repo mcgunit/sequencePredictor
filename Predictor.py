7d2d52a9aead1df461fd85173985a24e5a0ec648
mcgunit <michaelwellens@gmail.com>
2025-05-15 11:00:49 +0200

    added mising boost

--- Predictor.py
+++ Predictor.py
@@ -785,7 +785,7 @@ if __name__ == "__main__":
     )
 
     parser.add_argument('-r', '--rebuild_history', type=bool, default=False)
-    parser.add_argument('-d', '--days', type=int, default=3)
+    parser.add_argument('-d', '--days', type=int, default=183)
     args = parser.parse_args()
 
     print_intro()
