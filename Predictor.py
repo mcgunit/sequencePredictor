9780f1abb737fdc81c4d9e51482258a93e7ffd9b
mcgunit <michaelwellens@gmail.com>
2025-05-20 12:42:11 +0200

    set 7 days

--- Predictor.py
+++ Predictor.py
@@ -825,7 +825,7 @@ if __name__ == "__main__":
     )
 
     parser.add_argument('-r', '--rebuild_history', type=bool, default=False)
-    parser.add_argument('-d', '--days', type=int, default=5)
+    parser.add_argument('-d', '--days', type=int, default=7)
     args = parser.parse_args()
 
     print_intro()
