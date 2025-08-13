d9b1022b921fbd89d5aea3e485419160c1f7649b
michaelwellens <michael@airobot.eu>
2025-08-13 21:53:24 +0200

    saving last predictions

--- Predictor.py
+++ Predictor.py
@@ -858,7 +858,7 @@ if __name__ == "__main__":
         )
 
         parser.add_argument('-r', '--rebuild_history', type=bool, default=False)
-        parser.add_argument('-d', '--days', type=int, default=7)
+        parser.add_argument('-d', '--days', type=int, default=31)
         args = parser.parse_args()
 
         print_intro()
