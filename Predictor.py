ca36c9f029e21907ef3e5defb2b74ee2c574aa04
mcgunit <michaelwellens@gmail.com>
2026-01-15 11:28:05 +0100

    saving last predictions

--- Predictor.py
+++ Predictor.py
@@ -949,7 +949,8 @@ if __name__ == "__main__":
                             gameName = "Viking+Lotto"
                         dataFetcher.getLatestData(gameName, filePath)
                         #os.remove(os.path.join(dataPath, file))
-                    #command.run("wget -P {folder} https://prdlnboppreportsst.blob.core.windows.net/legal-reports/{file}".format(**kwargs_wget), verbose=False)
+                    else:
+                        command.run("wget -P {folder} https://prdlnboppreportsst.blob.core.windows.net/legal-reports/{file}".format(**kwargs_wget), verbose=False)
 
                     # Predict with hyperopt params
                     predict(dataset_name, model_type, dataPath, modelPath, skipLastColumns=skip_last_columns, daysToRebuild=daysToRebuild, ai=ai, boost=boost)
